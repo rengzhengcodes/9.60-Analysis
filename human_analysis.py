94a589c9fed8f647e2ab68540959c8654f090219
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-05-04 11:20:20 -0400

    collapsed data

--- human_analysis.py
+++ human_analysis.py
@@ -95,20 +95,55 @@ for person in data:
         pics += 1
         # notes the image shown to the response
         homologated_data[int(pic["background_difficulty"])][pic["category"]][choices[pic["response"]]] += 1
-
+"""
 # prints out the homologated data
 pp.pprint(homologated_data)
 
 # prints out the overall accuracy
 print(f"Accuracy: {sum(entry[choice][choice] for entry in homologated_data for choice in choices)/pics}")
-
+"""
 # tracks number of participants
 n: int = 98
 # pics per participant
 p: int = 8
-
+"""
 # calculates accuracy by difficulty
 i: int
 entry: dict[dict[str, int]]
 for i, entry in enumerate(homologated_data):
     print(f"Difficulty {i}: {sum(entry[choice][choice] for choice in choices)/(n*p)}")
+"""
+# collapses the difficulties into 1 large map
+collapsed: dict[dict[str, int]] = {}
+
+# initializes the collapsed dictionary
+shown: str
+for shown in choices:
+
+    # initializes for a shown stimulus
+    collapsed[shown] = {}
+
+    response: str
+    for response in choices:
+
+        # initializes response tally
+        collapsed[shown][response] = 0
+
+# collapses all entries
+entry: dict[dict[str, int]]
+for entry in homologated_data:
+
+    # pulls out a given stimulus's responses
+    shown: str
+    responses: dict[str, int]
+    for shown, responses in entry.items():
+        
+        # extracts the response + amount of response
+        response: str
+        num_response: int
+        for response, num_response in responses.items():
+
+            # adds to collapsed tally
+            collapsed[shown][response] += num_response
+
+print(collapsed)
